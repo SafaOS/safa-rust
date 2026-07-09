ce02dfc426244e90ab5d971c95cdca8a9f60e480
Safi <116169309+safiworks@users.noreply.github.com>
2026-07-09 08:29:46 +0300

    fix(clone.sh): use --filter instead of --depth

--- clone.sh
+++ clone.sh
@@ -6,10 +6,10 @@ cd rust
 git init
 git remote add origin https://github.com/rust-lang/rust
 
-git fetch --depth 1 origin 31fca3adb283cc9dfd56b49cdee9a96eb9c96ffd
+git fetch --filter=blob:none origin 31fca3adb283cc9dfd56b49cdee9a96eb9c96ffd
 git checkout 31fca3a
 git switch -c safa-stable
 git apply ../*.patch
 
 cp ../bootstrap-gh.toml bootstrap.toml
-git submodule update --init --depth 1 --recursive
+git submodule update --init --filter=blob:none --recursive
