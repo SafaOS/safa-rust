3f2f5a725e0a0c438f991620971b6ad0f7fc705b
safiworks <116169309+safiworks@users.noreply.github.com>
2026-07-09 08:07:00 +0300

    init

---
+++ clone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir rust
+cd rust
+
+git init
+git remote add origin https://github.com/rust-lang/rust
+
+git fetch --depth 1 origin 31fca3adb283cc9dfd56b49cdee9a96eb9c96ffd
+git checkout 31fca3a
+git switch -c safa-stable
+git apply ../*.patch
+
+cp ../bootstrap-gh.toml bootstrap.toml
